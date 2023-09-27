4b0d61e5a63d454d19987fef95bc1a9cfbcdb3f6
Maneendra Burrannagari <maneendra@Maneendras-MacBook-Air.local>
2023-09-27 19:11:08 -0400

    validation of changing team

--- index.py
+++ index.py
@@ -1,27 +1,7 @@
 from map import map
 from utils import get_matches_of_fav_team
 from about import about_app
-import pdb; 
-
-
-def choose_teams_menu():
-    """Displays the menu of available teams and lets user pick his favorite team and returns the favorite team"""
-    """
-    """
-    print("------ List of Currently Playing Teams ------")
-    print('')
-    print('Gujarat Titans (GT)')
-    print('Chennai Super Kings (CSK)')
-    print('Lucknow Super Giants (LSG)')
-    print('Mumbai Indians (MI)')
-    print('Rajasthan Royals (RR)')
-    print('Royal Challengers Banglore (RCB)')
-    print('Kolkata Knight Riders (KKR)')
-    print('Punjab Kings (PKS)')
-    print('Delhi Capitals (DC)')
-    print('Sunrisers Hyderabad (SRH)')
-    print('')
-
+from menus import choose_teams_menu,main_menu,first_sub_menu,second_sub_menu
 
 def view_win_loss_stats_fav_team(teamA,teamAList,year1,year2):
         """Returns a tuple with no. of wins, losses and no result matches of teamA between year1 and year2"""
@@ -43,45 +23,8 @@ def view_win_loss_stats_fav_team(teamA,teamAList,year1,year2):
         return (number_of_wins,number_of_losses,no_result)
 
 
-
-def main_menu(fav_team):
-    """Displays menu with the stats available for the player to view"""
-    print('')
-    print('----- Main Menu -----')
-    print(f'1. Stats of your team {fav_team}')
-    print(f'2. Stats of {fav_team} against another team')
-    print(f'3. Change your favorite team')
-    print(f'4. Exit')
-
-    opt = input('Enter options between 1-4: ')
-    
-    return int(opt)
-
-def first_sub_menu(fav_team):
-    print(f'----------- Self Analysis of your team {fav_team} --------------')
-    print(f'1. Team performance (Win%, Number of matches won/lost)')
-    print(f'2. MOTM Records (Man of the match records)')
-    print(f'3. Playoffs record')
-    print(f'4. Go back to Main Menu')
-
-    opt = input('Enter options between 1-4: ')
-    
-    return int(opt)
-
-
-def second_sub_menu(fav_team, teamB):
-    print(f'----------- Performance Against Other Team --------------')
-    print(f'1. Team Performance of {fav_team} against {teamB}')
-    print(f'2. How does {fav_team} stand against {teamB} in playoffs')
-    print(f'3. Change the team you would like to compare against')
-    print(f'4. Go back to Main Menu')
-
-    opt = input('Enter options between 1-3: ')
-
-    return int(opt)
-
 def motm(teamA,teamAList,year1,year2):
-
+    """ Returns an object containing the name of player and number of times they have won man of the match awards between year 1 and year 2 """
     # stores the number of man of the match awardees
     motm = {}
 
@@ -115,6 +58,12 @@ def playoff_record(teamAList):
             qualifiers.append((ele))
     return qualifiers
 
+def validate_team_input(team):
+    if (team not in map.keys()):
+        print(f'Invalid input, please select from the list of available teams {list(map.keys())}')
+        return
+    return True
+
 def main():
     try:
         # about the app
@@ -127,11 +76,7 @@ def main():
 
             fav_team = input('Enter the team you would like to view stats : ')
 
-            # if user enters invalid team or unsupported
-            if (fav_team not in map.keys()):
-                print(f'Invalid input, please select from the list of available teams {map.keys()}')
-
-            else:
+            if (validate_team_input(fav_team)):
                 fav_team = map[fav_team]
 
                 fav_team_list = get_matches_of_fav_team(fav_team)
@@ -220,123 +165,126 @@ def main():
                     elif (opt == 2):
                         # the second sub menu
                         teamB = input('Enter the team against which you would like to view stats: ')
-                        #normalize input data
-                        teamB =map[teamB]
 
-                        while (True):
-                            print()
+                        if (validate_team_input(teamB)):
+                            #normalize input data
+                            teamB = map[teamB]
 
-                            #second sub menu
-                            second_sub_option = second_sub_menu(fav_team, teamB)
+                            while (True):
+                                print()
+                                #second sub menu
+                                second_sub_option = second_sub_menu(fav_team, teamB)
 
-                            # 2.1 performance against other team
-                            if (second_sub_option == 1):
-                                toss_wins = 0
-                                head_head_playoffs = 0
-                                playoff_wins = 0
-                                number_of_wins = 0
-                                number_of_losses = 0
+                                # 2.1 performance against other team
+                                if (second_sub_option == 1):
+                                    toss_wins = 0
+                                    head_head_playoffs = 0
+                                    playoff_wins = 0
+                                    number_of_wins = 0
+                                    number_of_losses = 0
 
-                                print("{:<5} {:<30} {:<30} {:<30} {:<30} {:<30}".format("Year","First Team","Second Team","Toss winner","Winning Team","Venue"))
-                                print("-" * 165)
-                                for ele in fav_team_list:
-                                    if (ele[5] == teamB or ele[6] == teamB):
-                                        
-                                        print("{:<5} {:<30} {:<30} {:<30} {:<30} {:<30}".format(ele[3], ele[5],ele[6],ele[8],ele[11],ele[7]))
-                                        
-                                        if (ele[11] == fav_team):
-                                            number_of_wins+=1
-                                        
-                                        if (ele[11] == teamB):
-                                            number_of_losses+=1
-                                        
-                                        if (ele[8] == fav_team):
-                                            toss_wins+=1
-
-                                        if (ele[4] == 'Qualifier' or ele[4] == 'Final'):
-                                            head_head_playoffs+=1
+                                    print("{:<5} {:<30} {:<30} {:<30} {:<30} {:<30}".format("Year","First Team","Second Team","Toss winner","Winning Team","Venue"))
+                                    print("-" * 165)
+                                    for ele in fav_team_list:
+                                        if (ele[5] == teamB or ele[6] == teamB):
+                                            
+                                            print("{:<5} {:<30} {:<30} {:<30} {:<30} {:<30}".format(ele[3], ele[5],ele[6],ele[8],ele[11],ele[7]))
+                                            
+                                            if (ele[11] == fav_team):
+                                                number_of_wins+=1
+                                            
                                             if (ele[11] == teamB):
-                                                playoff_wins+=1
-                                print("")
-                                total_matches = number_of_wins + number_of_losses
+                                                number_of_losses+=1
+                                            
+                                            if (ele[8] == fav_team):
+                                                toss_wins+=1
 
-                                if(total_matches != 0):                     
-                                    print(f'{fav_team} has played {total_matches} matches against {teamB}')
-                                    print(f'{fav_team} has won {number_of_wins} head-to-head contests against {teamB}')
+                                            if (ele[4] == 'Qualifier' or ele[4] == 'Final'):
+                                                head_head_playoffs+=1
+                                                if (ele[11] == teamB):
+                                                    playoff_wins+=1
+                                    print("")
+                                    total_matches = number_of_wins + number_of_losses
 
-                                    print(f'Win % of {fav_team} against {teamB}: \t{round((number_of_wins/total_matches)*100)}%')
-                                    print(f'Win % tosses of {fav_team} against {teamB}: \t{round((toss_wins/total_matches*100))}%')
+                                    if(total_matches != 0):                     
+                                        print(f'{fav_team} has played {total_matches} matches against {teamB}')
+                                        print(f'{fav_team} has won {number_of_wins} head-to-head contests against {teamB}')
+
+                                        print(f'Win % of {fav_team} against {teamB}: \t{round((number_of_wins/total_matches)*100)}%')
+                                        print(f'Win % tosses of {fav_team} against {teamB}: \t{round((toss_wins/total_matches*100))}%')
+                                    else:
+                                        print(f'No games played between {fav_team} and {teamB}')
+
+                                # playoff record of both the teams
+                                elif (second_sub_option == 2):
+                                    # playoff record
+                                    print("{:<5} {:<20} {:<40} {:<40} {:<40} {:<40}".format("Year", "Playoff Match","First Team","Second Team","Winning Team","Venue"))
+                                    print("-" * 185)
+                                    
+                                    teamA_cup = 0
+                                    teamA_playoffs = 0
+
+                                    teamB_playoffs = 0
+                                    teamB_cup = 0
+
+                                    both_playoffs = 0
+
+                                    for ele in fav_team_list:
+                                        # filtering items where teamB and teamA have played and also its a qualifier match(playoff)
+                                        if ((ele[5] == teamB or ele[6] == teamB) and (ele[4] == 'Qualifier' or ele[4] == 'Final')):
+                                            both_playoffs+=1
+
+                                            print("{:<5} {:<20} {:<40} {:<40} {:<40} {:<40}".format(ele[3], ele[4], ele[5], ele[6],ele[11],ele[7]))
+                                        
+                                        if ((ele[4] == 'Qualifier' or ele[4] == 'Final')):
+                                            teamA_playoffs+=1
+                                            if (ele[11] == fav_team and ele[4] == 'Final'):
+                                                teamA_cup+=1
+                                    
+                                    teamBResults = get_matches_of_fav_team(teamB)
+
+                                    for ele in teamBResults:                                                     
+                                        if ((ele[4] == 'Qualifier' or ele[4] == 'Final')):
+                                            teamB_playoffs+=1
+                                            if (ele[11] == teamB and ele[4] == 'Final'):
+                                                teamB_cup+=1
+
+                                        
+                                    print("")
+
+                                    print(f'{fav_team} and {teamB} went head-to-head in {both_playoffs} playoff matches')
+                                    print(f'{fav_team} has won {teamA_cup} IPL tournaments')
+                                    print(f'{teamB} has won {teamB_cup} IPL tournaments')
+                                    print('')
+                                    print(f'{fav_team} has played {teamA_playoffs} playoff matches')
+                                    print(f'{teamB} has played {teamB_playoffs} playoff matches')
+                                        
+                                # switch to another teamB
+                                elif (second_sub_option == 3):
+                                    print("--- Switch teamB ---")
+                                    print("Select the opposing team you'd prefer to use for comparison")
+                                    choose_teams_menu()
+                                    teamB = input('Enter the opposing team: ')
+                                    teamB = map[teamB]
+                                    
+                                elif(second_sub_option == 4):
+                                    break
+                                
                                 else:
-                                    print(f'No games played between {fav_team} and {teamB}')
-
-                            # playoff record of both the teams
-                            elif (second_sub_option == 2):
-                                # playoff record
-                                print("{:<5} {:<20} {:<40} {:<40} {:<40} {:<40}".format("Year", "Playoff Match","First Team","Second Team","Winning Team","Venue"))
-                                print("-" * 185)
-                                
-                                teamA_cup = 0
-                                teamA_playoffs = 0
-
-                                teamB_playoffs = 0
-                                teamB_cup = 0
-
-                                both_playoffs = 0
-
-                                for ele in fav_team_list:
-                                    # filtering items where teamB and teamA have played and also its a qualifier match(playoff)
-                                    if ((ele[5] == teamB or ele[6] == teamB) and (ele[4] == 'Qualifier' or ele[4] == 'Final')):
-                                        both_playoffs+=1
-
-                                        print("{:<5} {:<20} {:<40} {:<40} {:<40} {:<40}".format(ele[3], ele[4], ele[5], ele[6],ele[11],ele[7]))
-                                    
-                                    if ((ele[4] == 'Qualifier' or ele[4] == 'Final')):
-                                        teamA_playoffs+=1
-                                        if (ele[11] == fav_team and ele[4] == 'Final'):
-                                            teamA_cup+=1
-                                
-                                teamBResults = get_matches_of_fav_team(teamB)
-
-                                for ele in teamBResults:                                                     
-                                    if ((ele[4] == 'Qualifier' or ele[4] == 'Final')):
-                                        teamB_playoffs+=1
-                                        if (ele[11] == teamB and ele[4] == 'Final'):
-                                            teamB_cup+=1
-
-                                    
-                                print("")
-
-                                print(f'{fav_team} and {teamB} went head-to-head in {both_playoffs} playoff matches')
-                                print(f'{fav_team} has won {teamA_cup} IPL tournaments')
-                                print(f'{teamB} has won {teamB_cup} IPL tournaments')
-                                print('')
-                                print(f'{fav_team} has played {teamA_playoffs} playoff matches')
-                                print(f'{teamB} has played {teamB_playoffs} playoff matches')
-                                    
-                            # switch to another teamB
-                            elif (second_sub_option == 3):
-                                print("--- Switch teamB ---")
-                                print("Select the opposing team you'd prefer to use for comparison")
-                                choose_teams_menu()
-                                teamB = input('Enter the opposing team: ')
-                                teamB = map[teamB]
-                                
-                            elif(second_sub_option == 4):
-                                break
-                            
-                            else:
-                                print('Invalid option. Please choose between 1-4')
+                                    print('Invalid option. Please choose between 1-4')
 
                     elif (opt == 3):
                         print('----- Change your primary team --------')
                         choose_teams_menu()
                         fav_team = input('Enter the team you would like to change to :')
-                        # if user enters invalid team or unsupported
-                        if (fav_team not in map.keys()):
-                            print(f'Invalid input, please select from the list of available teams {map.keys()}')
 
-                        fav_team = map[fav_team]
-                        fav_team_list = get_matches_of_fav_team(fav_team)
+                        if (validate_team_input(fav_team)):
+                            # if user enters invalid team or unsupported
+                            if (fav_team not in map.keys()):
+                                print(f'Invalid input, please select from the list of available teams {map.keys()}')
+
+                            fav_team = map[fav_team]
+                            fav_team_list = get_matches_of_fav_team(fav_team)
 
                     elif (opt == 4):
                         exit()
---
+++ menus.py
@@ -0,0 +1,54 @@
+def choose_teams_menu():
+    """Displays the menu of available teams and lets user pick his favorite team and returns the favorite team"""
+    """
+    """
+    print("------ List of Currently Playing Teams ------")
+    print('')
+    print('Gujarat Titans (GT)')
+    print('Chennai Super Kings (CSK)')
+    print('Lucknow Super Giants (LSG)')
+    print('Mumbai Indians (MI)')
+    print('Rajasthan Royals (RR)')
+    print('Royal Challengers Banglore (RCB)')
+    print('Kolkata Knight Riders (KKR)')
+    print('Punjab Kings (PKS)')
+    print('Delhi Capitals (DC)')
+    print('Sunrisers Hyderabad (SRH)')
+    print('')
+
+def main_menu(fav_team):
+    """Displays menu with the stats available for the player to view"""
+    print('')
+    print('----- Main Menu -----')
+    print(f'1. Stats of your team {fav_team}')
+    print(f'2. Stats of {fav_team} against another team')
+    print(f'3. Change your favorite team')
+    print(f'4. Exit')
+
+    opt = input('Enter options between 1-4: ')
+    
+    return int(opt)
+
+
+def first_sub_menu(fav_team):
+    print(f'----------- Self Analysis of your team {fav_team} --------------')
+    print(f'1. Team performance (Win%, Number of matches won/lost)')
+    print(f'2. MOTM Records (Man of the match records)')
+    print(f'3. Playoffs record')
+    print(f'4. Go back to Main Menu')
+
+    opt = input('Enter options between 1-4: ')
+    
+    return int(opt)
+
+
+def second_sub_menu(fav_team, teamB):
+    print(f'----------- Performance Against Other Team --------------')
+    print(f'1. Team Performance of {fav_team} against {teamB}')
+    print(f'2. How does {fav_team} stand against {teamB} in playoffs')
+    print(f'3. Change the team you would like to compare against')
+    print(f'4. Go back to Main Menu')
+
+    opt = input('Enter options between 1-3: ')
+
+    return int(opt)
