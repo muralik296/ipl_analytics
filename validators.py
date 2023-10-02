2d8875d31857a5fbf18ca2603270715510a84a6b
Maneendra Burrannagari <maneendra@Maneendras-Air.lan>
2023-10-02 17:29:26 -0400

    some changes

--- validators.py
+++ validators.py
@@ -3,9 +3,13 @@ from customErrorClass import InvalidInput
 
 
 def validate_team_input(teamA):
-    if (teamA not in map.keys()):
-        raise InvalidInput(f'Invalid input, please select from the list of available teams {list(map.keys())}')
-    return True
+    try:
+        if (teamA not in map.keys()):
+            raise InvalidInput(f'Invalid input, please select from the list of available teams {list(map.keys())}')
+        return True
+    except InvalidInput as e:
+        print(e)
+        return False
 
 def validate_change_teams(teamA,teamB):
     try:
